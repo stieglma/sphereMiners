d951947ca693c266e413f987137fe277fee30d90
Thomas Stieglmaier <thomas@stieglmaier.me>
2015-07-16 23:03:59 +0200

    more code in main

--- src/me/stieglmaier/sphereMiners/main/SphereMiners.java
+++ src/me/stieglmaier/sphereMiners/main/SphereMiners.java
@@ -1,10 +1,31 @@
 package me.stieglmaier.sphereMiners.main;
 
+import java.awt.EventQueue;
+
+/**
+ * Main class of the program. Creates the model and the View in a new Thread and
+ * adds the View as Observer to the model.
+ */
 public class SphereMiners {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    /**
+     * Launches the application.
+     *
+     * @param args
+     *            command line parameters are unused
+     */
+    public static void main(final String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                // new gui
+            }
+        });
+    }
 
+    /**
+     * Private constructor to prevent wrong initialization.
+     */
+    private SphereMiners() {
     }
 
 }
