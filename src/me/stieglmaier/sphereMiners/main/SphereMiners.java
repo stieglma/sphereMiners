231f780b393f3b0a520b94a3da214cbf1ed7da60
Thomas Stieglmaier <thomas@stieglmaier.me>
2015-07-19 00:40:45 +0200

    some changes

--- src/me/stieglmaier/sphereMiners/main/SphereMiners.java
+++ src/me/stieglmaier/sphereMiners/main/SphereMiners.java
@@ -1,58 +1,64 @@
 package me.stieglmaier.sphereMiners.main;
 
-import java.awt.EventQueue;
-import java.io.IOException;
-import java.net.MalformedURLException;
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import me.stieglmaier.sphereMiners.model.Model;
 import me.stieglmaier.sphereMiners.model.ai.AIManager;
 import me.stieglmaier.sphereMiners.model.physics.PhysicsManager;
 import me.stieglmaier.sphereMiners.view.GUI;
-
 import org.sosy_lab.common.configuration.Configuration;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 /**
  * Main class of the program. Creates the model and the View in a new Thread and
  * adds the View as Observer to the model.
  */
-public class SphereMiners {
+public class SphereMiners extends Application {
+
+    private static Model model;
 
     /**
      * Launches the application.
      *
      * @param args
      *            command line parameters are unused
-     * @throws IOException 
-     * @throws InvalidConfigurationException 
+     * @throws IOException
+     * @throws InvalidConfigurationException
      */
     public static void main(final String[] args) throws InvalidConfigurationException, IOException {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parameters params = getParameters();
 
         Configuration config;
-        if (args.length > 0) {
-            config = Configuration.builder().loadFromFile(args[0]).build();
+        if (params.getUnnamed().size() > 0) {
+            config = Configuration.builder().loadFromFile(params.getUnnamed().get(0)).build();
         } else {
             config = Configuration.defaultConfiguration();
         }
 
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                Model model = null;
-                try {
-                    model = new Model(new PhysicsManager(config), new AIManager(config));
-                } catch (ClassNotFoundException | MalformedURLException | InvalidConfigurationException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                new GUI(model);
-            }
-        });
-    }
+        Model model = null;
+        try {
+            model = new Model(new PhysicsManager(config), new AIManager(config));
+        } catch (ClassNotFoundException | MalformedURLException | InvalidConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-    /**
-     * Private constructor to prevent wrong initialization.
-     */
-    private SphereMiners() {
+        System.out.println(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Sphere Miners");
+        primaryStage.setScene(new Scene(new GUI(model)));
+        primaryStage.show();
     }
 
 }
--- src/me/stieglmaier/sphereMiners/view/GUI.java
+++ src/me/stieglmaier/sphereMiners/view/GUI.java
@@ -1,38 +1,22 @@
 package me.stieglmaier.sphereMiners.view;
 
+import javafx.scene.Parent;
+import me.stieglmaier.sphereMiners.model.Model;
+
 import java.util.Observable;
 import java.util.Observer;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import me.stieglmaier.sphereMiners.model.Model;
-
-public class GUI extends Application implements Observer {
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -7595502010828226886L;
+public class GUI extends Parent implements Observer {
 
     public GUI(Model m) {
         m.addObserver(this);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+
 
     @Override
     public void update(Observable o, Object arg) {
         // TODO Auto-generated method stub
-        
+
     }
 }
