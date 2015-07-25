336d4b51c8e68f0eebf0d00eb8221acaa879e8b3
Thomas Stieglmaier <thomas@stieglmaier.me>
2015-07-25 20:29:01 +0200

    more comments for player class

--- src/main/java/me/stieglmaier/sphereMiners/model/Player.java
+++ src/main/java/me/stieglmaier/sphereMiners/model/Player.java
@@ -8,6 +8,12 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.paint.Color;
 
+/**
+ * Player class, represents an AI in the framework.
+ *
+ * @author stieglma
+ *
+ */
 public class Player {
 
     private final String internalAIName;
@@ -15,6 +21,15 @@ public class Player {
     private final IntegerProperty aiSize;
     private Color color;
 
+    /**
+     * Create a Player, needs the internally used name, and the initial size of
+     * the ai.
+     *
+     * @param internalAiName This will be the name used for identification in the
+     *                       framework, usually the classname, as it is unique
+     * @param aiSize this is the initial size of the player, usually the initial
+     *               sphere size from the constants class
+     */
     public Player(String internalAiName, int aiSize) {
         this.internalAIName = internalAiName;
         this.aiSize = new SimpleIntegerProperty(aiSize);
@@ -22,10 +37,20 @@ public class Player {
         displayName = new SimpleStringProperty(internalAiName);
     }
 
+    /**
+     * Returns the internally used name.
+     *
+     * @return the internal name used for representing this AI.
+     */
     public String getInternalName() {
         return internalAIName;
     }
 
+    /**
+     * Returns the name that can be set by the ai as a property.
+     *
+     * @return the name of the AI
+     */
     public ReadOnlyStringProperty getNameProperty() {
         return displayName;
     }
@@ -33,11 +58,18 @@ public class Player {
     /**
      * package private such that only model and the aimanager / the ai itself
      * may call this method
+     *
+     * @param name The name the player should have from now on.
      */
     void setName(String name) {
         displayName.setValue(name);
     }
 
+    /**
+     * Returns the color the player chose to have.
+     *
+     * @return The color of the AI
+     */
     public Color getColor() {
         return color;
     }
@@ -45,15 +77,19 @@ public class Player {
     /**
      * package private such that only model and the aimanager / the ai itself
      * may call this method
+     *
+     * @param The color the player should have from now on.
      */
     void setColor(Color newColor) {
         color = newColor;
     }
 
-    public int getSize() {
-        return aiSize.get();
-    }
-
+    /**
+     * The current size of the AI as a property (it can be listened for changes).
+     * The size is the sum of all spheres sizes owned by this player.
+     *
+     * @return The current size of the ai.
+     */
     public ReadOnlyIntegerProperty getSizeProperty() {
         return aiSize;
     }
@@ -61,11 +97,16 @@ public class Player {
     /**
      * package private such that only model and the aimanager
      * may call this method
+     *
+     * @param the new size this player should have
      */
     void setSize(int newSize) {
         aiSize.set(newSize);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public String toString() {
         return displayName.get() + " (" + aiSize.get() + ", " + color + ")";
     }
