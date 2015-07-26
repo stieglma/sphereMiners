b96b1469ec3542c859b4c2e8d504065e51c2df78
Thomas Stieglmaier <thomas@stieglmaier.me>
2015-07-26 14:35:46 +0200

    dumbai a little less dumb

--- src/main/java/DumbAI.java
+++ src/main/java/DumbAI.java
@@ -1,7 +1,8 @@
 
 
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import javafx.scene.paint.Color;
 import me.stieglmaier.sphereMiners.model.Position;
@@ -17,20 +18,36 @@ public class DumbAI extends SphereMiners2015 {
 
     @Override
     protected void playTurn() {
-        Sphere ownSphere = ownSpheres.iterator().next();
-        Iterator<Sphere> dotsIt = dots.iterator();
-        Sphere nextDot = dotsIt.next();
-        double minDist = ownSphere.getPosition().dist(nextDot.getPosition());
-        while (dotsIt.hasNext()) {
-            Sphere tmpSphere = dotsIt.next();
-            double tmpDist = ownSphere.getPosition().dist(tmpSphere.getPosition());
-            if (tmpDist < minDist) {
-                minDist = tmpDist;
-                nextDot = tmpSphere;
+        Sphere ownSphere = null;
+        Iterator<Sphere> ownIt = ownSpheres.iterator();
+        Map<Sphere, Position> newDirections = new HashMap<>();
+
+        // first check if we could split
+        for(Sphere s : ownSpheres) {
+            if (s.getSize() > getConstants().getMinSplittingsize()) {
+                split(s);
+                return;
             }
         }
-        final Position moveTo = nextDot.getPosition().sub(ownSphere.getPosition());
-        changeMoveDirection(ownSpheres.stream().collect(Collectors.toMap(s -> s, s -> moveTo)));
+
+        // if no split occured we change directions
+        while (ownIt.hasNext()) {
+            ownSphere = ownIt.next();
+            Iterator<Sphere> dotsIt = dots.iterator();
+            Sphere nextDot = dotsIt.next();
+            double minDist = ownSphere.getPosition().dist(nextDot.getPosition());
+            while (dotsIt.hasNext()) {
+                Sphere tmpSphere = dotsIt.next();
+                double tmpDist = ownSphere.getPosition().dist(tmpSphere.getPosition());
+                if (tmpDist < minDist) {
+                    minDist = tmpDist;
+                    nextDot = tmpSphere;
+                }
+            }
+            final Position moveTo = nextDot.getPosition().sub(ownSphere.getPosition());
+            newDirections.put(ownSphere, moveTo);
+        }
+        changeMoveDirection(newDirections);
     }
 
 }
